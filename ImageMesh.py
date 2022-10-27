1cb4280256949c4fd0cf5f8db7eba7d473c1fc77
Curtis <curtis.lacy@gmail.com>
2022-10-27 19:53:27 -0400

    Add sides of the generated ImageSolid, to form a watertight shape.

--- ImageMesh.py
+++ ImageMesh.py
@@ -10,7 +10,7 @@ def _create_surface(heights):
     height = len(heights)
     vertices = numpy.empty([height * width, 3])
     for row in range(0, len(heights)):
-        for column in range(0, len(heights[0])):
+        for column in range(0, width):
             index = row * width + column
             vertices[index][0] = row
             vertices[index][1] = column
@@ -41,10 +41,52 @@ def _create_surface(heights):
     }
 
 
-def _create_footprint(heights, thickness=1):
-    altitude = numpy.amin(heights) - thickness
-    bottom_heights = altitude * numpy.ones(numpy.shape(heights))
-    return ImageShell(bottom_heights)
+def _create_closing_surface(top, bottom, x_coordinates, y_coordinates):
+    width = len(x_coordinates)
+    height = 2
+    vertices = numpy.empty([width * height, 3])
+    for column in range(0, width):
+        vertices[column*2][0] = x_coordinates[column]
+        vertices[column*2][1] = y_coordinates[column]
+        vertices[column*2][2] = top[column]
+
+        vertices[column*2+1][0] = x_coordinates[column]
+        vertices[column*2+1][1] = y_coordinates[column]
+        vertices[column*2+1][2] = bottom[column]
+
+    quad_columns = width - 1
+    faces = numpy.empty([2 * quad_columns, 3], dtype=numpy.uintc)
+    for face in range(0, len(faces), 2):
+        quad = int(face / 2)
+        upper_left = quad*2
+        lower_left = (quad*2)+1
+        upper_right = (quad*2)+2
+        lower_right = (quad*2)+3
+
+        # lower triangle
+        faces[face][0] = upper_left
+        faces[face][1] = lower_right
+        faces[face][2] = lower_left
+
+        # upper angle
+        faces[face + 1][0] = upper_left
+        faces[face + 1][1] = upper_right
+        faces[face + 1][2] = lower_right
+
+    return _create_mesh({
+        "faces": faces,
+        "vertices": vertices
+    })
+
+
+def _create_mesh(shape):
+    faces = shape["faces"]
+    vertices = shape["vertices"]
+    surface = mesh.Mesh(numpy.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
+    for i, f in enumerate(faces):
+        for j in range(3):
+            surface.vectors[i][j] = vertices[f[j], :]
+    return surface
 
 
 class ImageShell(mesh.Mesh):
@@ -75,11 +117,44 @@ class ImageSolid(mesh.Mesh):
         # Make the shell for the image surface
         self.image_shell = ImageShell(heights)
         # Also make the shell for the footprint
-        self.footprint = _create_footprint(self.heights, thickness)
-        # TODO: Make four more shells for the walls.
+        self.footprint_heights = self._create_footprint(thickness)
+        self.footprint_shell = ImageShell(self.footprint_heights)
+
+        # Now make the four walls
+        self.top_edge = _create_closing_surface(
+            self.heights[0], self.footprint_heights[0],
+            numpy.zeros(len(heights[0])),
+            numpy.arange(0,len(heights[0]))
+        )
+        bottom_edge_index = len(self.heights)-1
+        self.bottom_edge = _create_closing_surface(
+            self.heights[bottom_edge_index], self.footprint_heights[bottom_edge_index],
+            bottom_edge_index * numpy.ones(len(self.heights[0])),
+            numpy.arange(0, len(heights[0]))
+        )
+        self.left_edge = _create_closing_surface(
+            self.heights[:, 0], self.footprint_heights[:, 0],
+            numpy.arange(0, len(heights)),
+            numpy.zeros(len(heights))
+        )
+        right_edge_index = len(self.heights[0])-1
+        self.right_edge = _create_closing_surface(
+            self.heights[:, right_edge_index], self.footprint_heights[:, right_edge_index],
+            numpy.arange(0, len(heights)),
+            right_edge_index * numpy.ones(len(heights))
+        )
 
         # Now combine all the surfaces to make the actual solid mesh
-        super().__init__(numpy.concatenate([self.image_shell.data, self.footprint.data]))
+        super().__init__(numpy.concatenate([self.image_shell.data,
+                                            self.footprint_shell.data,
+                                            self.top_edge.data,
+                                            self.bottom_edge.data,
+                                            self.left_edge.data,
+                                            self.right_edge.data]))
+
+    def _create_footprint(self, thickness=1):
+        altitude = numpy.amin(self.heights) - thickness
+        return altitude * numpy.ones(numpy.shape(self.heights))
 
 
 def display_as_3d(mesh, xlim=None, ylim=None, zlim=None, elevation=45., azimuth=45.):
